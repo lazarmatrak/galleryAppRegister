c7c2bc5f51a70fc2347362d0dba335cd93347d8d
lazar <lazarmatrak@mail.ru>
2023-05-22 22:54:55 +0200

    add positive and negative test cases for register

--- cypress/e2e/registerSpecs/register.cy.js
+++ cypress/e2e/registerSpecs/register.cy.js
@@ -1,11 +1,9 @@
 /// <reference types="cypress" />
-describe('Gallery App Registration test suite',()=>{
+const password = 'newpassword123';
+describe('Gallery App Registration test suite - POSITIVE',()=>{
     beforeEach(()=>{
         cy.visit(Cypress.config('baseUrl'))
     })
-    const password = 'newpassword123';
-    // Happy Path
-
     it('Successfully register user',()=>{
         cy.get('#first-name').type('Lazar')
         cy.get('#last-name').type('Matrak')
@@ -14,7 +12,70 @@ describe('Gallery App Registration test suite',()=>{
         cy.get('#password-confirmation').type(password)
         cy.get('.form-check-input').check()
         cy.get('.btn').click()
+    })    
+})
+
+describe('Gallery App Registration test suite - NEGATIVE', () => {
+    beforeEach(()=>{
+        cy.visit(Cypress.config('baseUrl'))
     })
 
-    // Sad Path
+    it('First Name - Empty field',()=>{
+        cy.get('#last-name').type('Matrak')
+        cy.get('#email').type('warclock@gmail.com')
+        cy.get('#password').type(password)
+        cy.get('#password-confirmation').type(password)
+        cy.get('.form-check-input').check()
+        cy.get('.btn').click()
+        cy.wait(2000)
+    })
+
+    it('Last Name - Empty field',()=>{
+        cy.get('#first-name').type('Lazar')
+        cy.get('#email').type('warclock@gmail.com')
+        cy.get('#password').type(password)
+        cy.get('#password-confirmation').type(password)
+        cy.get('.form-check-input').check()
+        cy.get('.btn').click()
+        cy.wait(2000)
+    })
+
+    it('Email - Empty field',()=>{
+        cy.get('#first-name').type('Lazar')
+        cy.get('#last-name').type('Matrak')
+        cy.get('#password').type(password)
+        cy.get('#password-confirmation').type(password)
+        cy.get('.form-check-input').check()
+        cy.get('.btn').click()
+        cy.wait(2000)
+    })
+
+    it('Password - Empty field',()=>{
+        cy.get('#first-name').type('Lazar')
+        cy.get('#last-name').type('Matrak')
+        cy.get('#email').type('warclock@gmail.com')
+        cy.get('#password-confirmation').type(password)
+        cy.get('.form-check-input').check()
+        cy.get('.btn').click()
+        cy.wait(2000)
+    })
+
+    it('Password Confirmation - Empty field',()=>{
+        cy.get('#first-name').type('Lazar')
+        cy.get('#last-name').type('Matrak')
+        cy.get('#email').type('warclock@gmail.com')
+        cy.get('#password').type(password)
+        cy.get('.form-check-input').check()
+        cy.get('.btn').click()
+        cy.wait(2000)
+    })
+    
+    it('Terms and Agreement - Not check',()=>{
+        cy.get('#first-name').type('Lazar')
+        cy.get('#last-name').type('Matrak')
+        cy.get('#email').type('warclock@gmail.com')
+        cy.get('#password').type(password)
+        cy.get('#password-confirmation').type(password)
+        cy.wait(2000)
+    })
 })
